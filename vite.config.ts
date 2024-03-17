8bc15054698af625195c5dfe5bf7aa744ec8e64e
Jorge0620 <jorgemoralesupwork@gmail.com>
2024-03-16 17:08:27 -0700

    Modify source specification 1

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,9 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@/': `${path.resolve(__dirname, 'src')}/`,
-    },
-  },
+  // resolve: {
+  //   alias: {
+  //     '@/': `${path.resolve(__dirname, 'src')}/`,
+  //   },
+  // },
 })
